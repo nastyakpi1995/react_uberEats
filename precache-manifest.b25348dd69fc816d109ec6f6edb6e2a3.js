daefe59cf3aef92ff13124a3368009c4c0d134dd
Nastya Krat <nastyakpi1995@gmail.com>
2021-12-03 11:44:43 +0200

    Updates

---
+++ precache-manifest.b25348dd69fc816d109ec6f6edb6e2a3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff73762e5a0bb39c876d3909a2538e6d",
+    "url": "/react_uberEats/index.html"
+  },
+  {
+    "revision": "0f4a1da1489791622203",
+    "url": "/react_uberEats/static/css/main.20364511.chunk.css"
+  },
+  {
+    "revision": "eb6c82bcd58a46c4df25",
+    "url": "/react_uberEats/static/js/2.325cb78d.chunk.js"
+  },
+  {
+    "revision": "0f4a1da1489791622203",
+    "url": "/react_uberEats/static/js/main.b6a88e78.chunk.js"
+  },
+  {
+    "revision": "4467026129587387f997",
+    "url": "/react_uberEats/static/js/runtime~main.e33a8672.js"
+  },
+  {
+    "revision": "1b627d457ebe1b95b701b1b04809014b",
+    "url": "/react_uberEats/static/media/planet.1b627d45.svg"
+  },
+  {
+    "revision": "810c92dfd823e8b78bfaf8401b31473b",
+    "url": "/react_uberEats/static/media/search.810c92df.svg"
+  },
+  {
+    "revision": "18e5d81b95ec4e06bc149a58ebeb101b",
+    "url": "/react_uberEats/static/media/time.18e5d81b.svg"
+  }
+]);
